84f884cf0cc98a620bfc9f6b20dbef9488a455a8
ezikechris <christophermbe1009@gmail.com>
2021-01-19 01:57:19 +0100

    React App Changes

--- ReactNotes.js
+++ ReactNotes.js
@@ -23,3 +23,90 @@ YOU CREATE YOUR REACT APPLICATION WITH ITS NAME USING THE FOLLOWING COMMANDS
 create-react-app henchmen all small letters
 
 */
+
+
+///// GITIGNORE FILES TO ADD ///
+
+
+
+/*
+
+# See https://help.github.com/articles/ignoring-files/ for more about ignoring files.
+
+# dependencies
+/node_modules
+/.pnp
+.pnp.js
+
+# testing
+/coverage
+
+# production
+/build
+
+# misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
+
+
+*/
+
+/*
+
+
+
+When a new version of react has been created and you would love 
+to update your react app with the new version on the website you simply change the version id or number
+and run NPM INSTALL COMMAND
+
+
+
+
+*/
+
+/// REACT CODE TO BE USED WHEN WORKING WITH REACT APP.JS ///
+
+
+
+/*
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class  App extends React.Component {
+  render()  {
+    return (
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+    )
+  }
+    
+
+}
+
+export default App;
+
+
+*/
+
